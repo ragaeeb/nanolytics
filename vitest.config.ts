3457a0b786d0e017a0b188a62e3d7d0b7c62b1d8
Ragaeeb Haq <7464039+ragaeeb@users.noreply.github.com>
2024-10-20 19:42:54 +0400

    Update test config

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,5 +1,10 @@
 export default {
     test: {
+        coverage: {
+            provider: 'v8',
+            reporter: ['text', 'lcov'],
+            reportsDirectory: './coverage',
+        },
         include: ['src/**/*.test.ts'],
     },
 };
